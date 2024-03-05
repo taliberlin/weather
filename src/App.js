1e822e8717bed8223d454917d547fa23e8b0bd65
taliberlin <118308290+taliberlin@users.noreply.github.com>
2024-03-05 15:05:18 -0800

    styled and added links to footer

--- src/App.js
+++ src/App.js
@@ -10,8 +10,22 @@ function App() {
         <SearchBar />
       </main>
       <footer className="text-center mt-3">
-        This website was coded by Tali Berlin using React and is open-sourced on
-        GitHub
+        This website was coded by{" "}
+        <a
+          href="https://peppy-starburst-3356e6.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Tali Berlin
+        </a>{" "}
+        using React and is open-sourced on{" "}
+        <a
+          href="https://github.com/taliberlin/weather"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub ✌️
+        </a>
       </footer>
     </div>
   );
